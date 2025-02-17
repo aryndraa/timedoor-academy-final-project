bd1f60f60b063eb68817d5841a89a2c3793881f3
aryndraa <aryacoder1102@gmail.com>
2025-02-17 15:08:43 +0800

    set colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./_components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary : "#EABB40",
+        dark: {
+          100: "#42444C",
+          200: "#27282B",
+          300: "#212224",
+          400: "#42444C"
+        },
+
+        text: {
+          100: "#BCBCBC",
+          200: "#A6A7A7",
+          300: "#D2D2D2",
+          400: "#FFFFFF"
+        }
       },
     },
   },
