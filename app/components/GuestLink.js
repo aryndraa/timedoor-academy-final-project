f0e0092273a30f3df3dcec5336f394e9b8003479
aryndraa <aryacoder1102@gmail.com>
2025-02-18 22:38:25 +0800

    desktop header

--- app/components/GuestLink.js
+++ app/components/GuestLink.js
@@ -2,12 +2,12 @@ import { FaUserAlt } from "react-icons/fa";
 
 export const GuestLink = () => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 lg:gap-4">
       <div className="text-right text-white">
-        <h2 className="text-sm lg:text-lg leading-[1.5] font-semibold">Mahendra Arya</h2>
-        <p className="text-[11px] lg:text-sm text-white/80">aryacoder1102@gma...</p>
+        <h2 className="text-sm lg:text-base leading-[1.5] font-semibold">Mahendra Arya</h2>
+        <p className="text-[11px] lg:text-xs text-white/80">aryacoder1102@gma...</p>
       </div>
-      <div className="p-3 text-lg lg:text-xl bg-dark-100 text-white rounded-full">
+      <div className="p-3 text-lg lg:text-xl  bg-dark-100 text-white rounded-full">
         <FaUserAlt/>
       </div>
     </div>
--- app/components/Header.js
+++ app/components/Header.js
@@ -3,7 +3,7 @@ import { Logo } from "./Logo"
 
 export const Header = () => {
   return (
-    <header className="px-3 py-4 flex justify-between items-center ">
+    <header className="px-3 lg:px-20 py-4 flex justify-between items-center ">
         <Logo/>
         <GuestLink/>
     </header>
