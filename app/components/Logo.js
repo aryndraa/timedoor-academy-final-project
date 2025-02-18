f50b61ba0ba532b1ee7b60d888e46658fa228e2c
aryndraa <aryacoder1102@gmail.com>
2025-02-18 22:32:40 +0800

    dekstop logo

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -4,16 +4,16 @@ import Link from "next/link"
 
 export const Logo = () => { 
   return (
-    <Link href={"/"} className="flex items-center gap-2">
+    <Link href={"/"} className="flex items-center gap-2 lg:gap-3 ">
       <Image
         src={logo}
         quality={100}
         alt="logo"
-        width={36}
+        className="w-9 lg:w-11"
       />
       <div className="text-white">
-        <h1 className="text-xl font-semibold leading-[1.3]">Aurora</h1>
-        <p className="text-[10px] text-white/80">Cinema Asia</p>
+        <h1 className="text-xl lg:text-2xl font-semibold leading-[1.3]">Aurora</h1>
+        <p className="text-[10px] lg:text-sm text-white/80">Cinema Asia</p>
       </div>
     </Link>
   )
