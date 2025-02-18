bd921ec05afd08c0e5e5100994bccd448fb082b9
aryndraa <aryacoder1102@gmail.com>
2025-02-18 22:33:29 +0800

    dekstop guest link

--- app/components/GuestLink.js
+++ app/components/GuestLink.js
@@ -4,10 +4,10 @@ export const GuestLink = () => {
   return (
     <div className="flex items-center gap-2">
       <div className="text-right text-white">
-        <h2 className="text-sm leading-[1.5] font-semibold">Mahendra Arya</h2>
-        <p className="text-[11px] text-white/80">aryacoder1102@gma...</p>
+        <h2 className="text-sm lg:text-lg leading-[1.5] font-semibold">Mahendra Arya</h2>
+        <p className="text-[11px] lg:text-sm text-white/80">aryacoder1102@gma...</p>
       </div>
-      <div className="p-3 text-lg bg-dark-100 text-white rounded-full">
+      <div className="p-3 text-lg lg:text-xl bg-dark-100 text-white rounded-full">
         <FaUserAlt/>
       </div>
     </div>
