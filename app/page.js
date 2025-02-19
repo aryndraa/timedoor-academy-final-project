936a6463b9d6a66d03a09773142c67bfa0937c6f
aryndraa <aryacoder1102@gmail.com>
2025-02-20 01:51:48 +0800

    bg

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
       <section className="min-h-[60vh] flex flex-col gap-6 lg:gap-12 lg:items-center lg:justify-end py-8 ">
         <div className="-z-10 hidden lg:block">
           <Image src={cinemaImg} alt="hero" fill={true} className="max-h-[40vh] lg:max-h-[60vh] object-cover"/>
-          <div className="min-h-[40vh] lg:min-h-[60vh] absolute top-0 left-0 right-0 bg-gradient-to-b from-dark-300/10  to-dark-300/60"></div>
+          <div className="min-h-[40vh] lg:min-h-[60vh] absolute top-0 left-0 right-0 bg-gradient-to-b from-dark-300/10  to-dark-300"></div>
         </div>
         <div className="self-start">
           <h1 className="text-3xl  lg:text-7xl font-semibold text-white lg:mb-3">
