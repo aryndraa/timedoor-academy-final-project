cd13ef5d873058910b5c8133b3a90f11cd661287
aryndraa <aryacoder1102@gmail.com>
2025-02-19 23:55:03 +0800

    discover form layout

--- app/components/DiscoverMovieForm.js
+++ app/components/DiscoverMovieForm.js
@@ -2,8 +2,14 @@ import {DiscoverSelect} from "@/app/components/DiscoverSelect";
 
 export const DiscoverMovieForm = () => {
  return (
-   <form action="">
+   <form action="" className="flex flex-col gap-3 bg-dark-200 p-4">
      <DiscoverSelect/>
+     <DiscoverSelect/>
+     <div className="flex gap-3">
+      <DiscoverSelect/>
+      <DiscoverSelect/>
+
+     </div>
    </form>
  )
 }
