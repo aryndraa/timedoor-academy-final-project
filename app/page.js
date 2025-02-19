2672131b3150074ce2149094f570724d822906a3
aryndraa <aryacoder1102@gmail.com>
2025-02-20 01:51:01 +0800

    change color

--- app/page.js
+++ app/page.js
@@ -5,12 +5,16 @@ import { DiscoverMovieForm } from "./components/DiscoverMovieForm";
 export default function Home() {
   return (
     <>
-      <section className="min-h-[80vh] flex flex-col gap-6 lg:items-center lg:justify-center py-8 ">
-        <div className="lg:text-center">
-          <h1 className="text-3xl   lg:text-7xl font-semibold text-white lg:mb-4">
+      <section className="min-h-[60vh] flex flex-col gap-6 lg:gap-12 lg:items-center lg:justify-end py-8 ">
+        <div className="-z-10 hidden lg:block">
+          <Image src={cinemaImg} alt="hero" fill={true} className="max-h-[40vh] lg:max-h-[60vh] object-cover"/>
+          <div className="min-h-[40vh] lg:min-h-[60vh] absolute top-0 left-0 right-0 bg-gradient-to-b from-dark-300/10  to-dark-300/60"></div>
+        </div>
+        <div className="self-start">
+          <h1 className="text-3xl  lg:text-7xl font-semibold text-white lg:mb-3">
             Discover Movie
           </h1>
-          <p className="text-sm lg:text-lg">Search Movie, Cinema, City....</p>
+          <p className="text-sm lg:text-xl lg:text-white">Search Movie, Cinema, City....</p>
         </div>
         <div className="w-full">
           <DiscoverMovieForm/>
