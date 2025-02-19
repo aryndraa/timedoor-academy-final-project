96286184d717390d8a47d079e1f8ac227a446b68
aryndraa <aryacoder1102@gmail.com>
2025-02-19 22:50:52 +0800

    hero

--- app/page.js
+++ app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import cinemaImg from "@/app/_assets/cinema.jpg"
 
 export default function Home() {
   return (
-    <h1>
-      Home
-    </h1>
+    <>
+      <section className="min-h-[80vh] flex items-center justify-center ">
+        <div className="text-center">
+          <h1 className="text-7xl font-semibold text-white mb-4">
+            Discover Movie
+          </h1>
+          <p className="text-lg">Search Movie, Cinema, City....</p>
+        </div>
+      </section>
+    </>
   );
 }
