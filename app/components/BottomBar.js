c28aa434203af75de90b48212ae9ba2974985f4b
aryndraa <aryacoder1102@gmail.com>
2025-02-19 22:22:28 +0800

    header rounded

--- app/components/BottomBar.js
+++ app/components/BottomBar.js
@@ -2,7 +2,7 @@ import {Navigation} from "@/app/components/Navigation";
 
 export const BottomBar = () => {
   return (
-    <div className="fixed bottom-0 right-0 left-0 px-3 py-4 bg-dark-200 ">
+    <div className="fixed lg:hidden bottom-0 right-0 left-0 px-5 py-4 bg-dark-200 ">
       <Navigation/>
     </div>
   )
--- app/components/Header.js
+++ app/components/Header.js
@@ -4,8 +4,8 @@ import { Navigation } from "./Navigation"
 
 export const Header = () => {
   return (
-    <header className=" flex flex-col lg:bg-dark-200">
-      <div className="flex justify-between items-center py-6 px-3 lg:px-20 ">
+    <header className=" flex flex-col lg:bg-dark-200 mx-16 my-5 rounded-full">
+      <div className="flex justify-between items-center py-4 px-3 lg:px-12 ">
         <Logo/>
         <div className="hidden lg:flex">
           <Navigation/>
