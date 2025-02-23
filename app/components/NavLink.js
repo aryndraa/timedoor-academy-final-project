471a58f4a71f39ed555d21a776bcc25ce2364072
aryndraa <aryacoder1102@gmail.com>
2025-02-23 23:46:28 +0800

    optimize navlink

--- app/components/NavLink.js
+++ app/components/NavLink.js
@@ -1,15 +1,23 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export const NavLink = ({path, name, icon}) => {
-  const pathname = usePathname() 
+export const NavLink = ({ path, name, icon }) => {
+  const pathname = usePathname();
+
+  const isActive =
+    path === "/"
+      ? pathname === "/" 
+      : pathname.startsWith(path); 
   return (
-    <Link href={path} className={` text-[10px] lg:text-lg font-semibold lg:font-medium flex flex-col gap-1 items-center ${
-      pathname === path ? "text-primary" : "text-white/40"
-    }`}>
+    <Link
+      href={path}
+      className={`text-[10px] lg:text-lg font-semibold lg:font-medium flex flex-col gap-1 items-center ${
+        isActive ? "text-primary" : "text-white/40"
+      }`}
+    >
       <span className="lg:hidden text-2xl">{icon}</span>
       {name}
     </Link>
   );
-}
+};
