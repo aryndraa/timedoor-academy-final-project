ada8c0aef47a8710794b9fef77d6b266f28fe1f7
aryndraa <aryacoder1102@gmail.com>
2025-02-24 23:01:07 +0800

    tab

--- app/movies/layout.js
+++ app/movies/layout.js
@@ -9,12 +9,12 @@ export default function RootLayout({ children }) {
 
   return (
     <>
-     <section className="min-h-[60vh] flex flex-col  py-8 lg:mx-16 pb-12 lg:pb-24">
+     <section className="min-h-[60vh] flex flex-col  py-4 lg:mx-16 pb-12 lg:pb-24">
        <div className="flex justify-center gap-6">
-         <Link href={"/movies"}  className={`font-medium text-lg border-b-2  pb-4  ${
+         <Link href={"/movies"}  className={`font-medium  border-b-2  pb-3  ${
           pathname === "/movies" ? "border-primary text-primary" : "border-transparent"
          }`}>Playing Now</Link>
-         <Link href={"/movies/upcoming"} className={`font-medium text-lg border-b -2  pb-4 ${
+         <Link href={"/movies/upcoming"} className={`font-medium  border-b-2  pb-3 ${
           pathname === "/movies/upcoming" ? "border-primary text-primary" : "border-transparent"
          }`}>Upcoming Movies</Link>
        </div>
