b0611fe5ea676aa5f78c5676db6699e26c26eb22
aryndraa <aryacoder1102@gmail.com>
2025-02-25 22:58:53 +0800

    rounded search bar

--- app/movies/layout.js
+++ app/movies/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
     <>
      <section className="min-h-[60vh] flex flex-col  py-8 lg:mx-16 pb-12 lg:pb-24">
        <div className=" mb-8 lg:mb-12 flex flex-col lg:flex-row-reverse lg:justify-between lg:items-center gap-10">
-       <div className="rounded-full p-3 px-4 pl-6 bg-dark-200  w-full lg:max-w-[30%] flex justify-between items-center" >
+       <div className="rounded-lg p-3 px-4 pl-6 bg-dark-200  w-full lg:max-w-[30%] flex justify-between items-center" >
           <input type="text" className=" max-w-[80%] focus:outline-none bg-transparent  placeholder:text-text-200" placeholder="Search Movie..."/>
           <Link href={"/"} className="text-2xl  p-2 rounded-full bg-dark-100">
             <IoSearch/>
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         <div className="flex  gap-4 lg:gap-6">
           <Link  href={"/movies"}  className={`font-medium lg:text-lg  border-b-2  pb-3 lg:pb-4  ${
               pathname === "/movies" ? "border-primary text-primary" : "border-transparent"
-             }`}>Playng Now</Link>
+             }`}>Playing Now</Link>
           <Link  href={"/movies/upcoming"} className={`font-medium lg:text-lg  border-b-2  pb-3 lg:pb-4 ${
               pathname === "/movies/upcoming" ? "border-primary text-primary" : "border-transparent"
              }`}>Upcoming Movies</Link>
