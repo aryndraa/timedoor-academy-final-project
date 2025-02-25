6bfd0e6d1b3b53bb447ea0cd9106e2a1b0f1fa7c
aryndraa <aryacoder1102@gmail.com>
2025-02-25 23:58:21 +0800

    dynamic bottom bar

--- app/components/BottomBar.js
+++ app/components/BottomBar.js
@@ -1,9 +1,23 @@
+"use client"
 import {Navigation} from "@/app/components/Navigation";
+import { usePathname } from "next/navigation";
 
 export const BottomBar = () => {
+  const pathname = usePathname();
+  const isMovieDetailPage = /^\/movies\/[^/]+$/.test(pathname); // Untuk /movies/[movieId]
+  const isBookingPage = /^\/movies\/[^/]+\/book$/.test(pathname); // Untuk /movies/[movieId]/book
+
+  const shouldHideHeader = isMovieDetailPage || isBookingPage;
+
   return (
-    <div className="fixed lg:hidden bottom-0 right-0 left-0 px-5 py-4 bg-dark-200  z-50">
-      <Navigation/>
-    </div>
+    <>
+      {!shouldHideHeader ? (
+        <div className="fixed lg:hidden bottom-0 right-0 left-0 px-5 py-4 bg-dark-200  z-50">
+          <Navigation/>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
   )
 }
