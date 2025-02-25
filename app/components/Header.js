2f844848ac709a1e9acbfa439553cd38e28b5c83
aryndraa <aryacoder1102@gmail.com>
2025-02-26 00:01:04 +0800

    undo

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,33 +1,17 @@
-"use client"
-
-import { GuestLink } from "./GuestLink";
-import { Logo } from "./Logo";
-import { Navigation } from "./Navigation";
-import { usePathname } from "next/navigation";
+import { GuestLink } from "./GuestLink"
+import { Logo } from "./Logo"
+import { Navigation } from "./Navigation"
 
 export const Header = () => {
-  const pathname = usePathname();
-
-  const isMovieDetailPage = /^\/movies\/[^/]+$/.test(pathname); // Untuk /movies/[movieId]
-  const isBookingPage = /^\/movies\/[^/]+\/book$/.test(pathname); // Untuk /movies/[movieId]/book
-
-  const shouldHideHeader = isMovieDetailPage || isBookingPage;
-
   return (
-    <>
-      {!shouldHideHeader ? (
-        <header className="flex flex-col lg:bg-dark-200 lg:mx-16 lg:my-5 rounded-full">
-          <div className="flex justify-between items-center py-4 px-3 lg:px-12">
-            <Logo />
-            <div className="hidden lg:flex">
-              <Navigation />
-            </div>
-            <GuestLink />
-          </div>
-        </header>
-      ) : (
-        <header></header>
-      )}
-    </>
-  );
-};
+    <header className=" flex flex-col lg:bg-dark-200 lg:mx-16 lg:my-5 rounded-full">
+      <div className="flex justify-between items-center py-4 px-3 lg:px-12 ">
+        <Logo/>
+        <div className="hidden lg:flex">
+          <Navigation/>
+        </div>
+        <GuestLink/>
+      </div>
+    </header>
+  )
+}
