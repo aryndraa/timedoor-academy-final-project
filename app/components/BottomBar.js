17106063cbb5b196738e0120ad357f6db45a8c78
aryndraa <aryacoder1102@gmail.com>
2025-02-26 00:01:15 +0800

    undo

--- app/components/BottomBar.js
+++ app/components/BottomBar.js
@@ -1,23 +1,9 @@
-"use client"
 import {Navigation} from "@/app/components/Navigation";
-import { usePathname } from "next/navigation";
 
 export const BottomBar = () => {
-  const pathname = usePathname();
-  const isMovieDetailPage = /^\/movies\/[^/]+$/.test(pathname); // Untuk /movies/[movieId]
-  const isBookingPage = /^\/movies\/[^/]+\/book$/.test(pathname); // Untuk /movies/[movieId]/book
-
-  const shouldHideHeader = isMovieDetailPage || isBookingPage;
-
   return (
-    <>
-      {!shouldHideHeader ? (
-        <div className="fixed lg:hidden bottom-0 right-0 left-0 px-5 py-4 bg-dark-200  z-50">
-          <Navigation/>
-        </div>
-      ) : (
-        <div></div>
-      )}
-    </>
+    <div className="fixed lg:hidden bottom-0 right-0 left-0 px-5 py-4 bg-dark-200  z-50">
+      <Navigation/>
+    </div>
   )
 }
