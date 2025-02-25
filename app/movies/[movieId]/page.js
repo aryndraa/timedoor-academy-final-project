ed432a60668e313c944002cb798ef3f95930c3e9
aryndraa <aryacoder1102@gmail.com>
2025-02-26 00:09:08 +0800

    test

--- app/movies/[movieId]/page.js
+++ app/movies/[movieId]/page.js
@@ -1,7 +1,16 @@
-export default function  Page ()  {
+import { MovieCard } from "@/app/components/cards/MovieCard";
 
+export default function Page ()  {
   return (
-    <div>
-    </div>
+    <>
+         <div className="grid grid-cols-2  lg:grid-cols-4 gap-4 gap-y-8 lg:gap-8">
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+            <MovieCard/>
+          </div>
+    </>
   )
 }
