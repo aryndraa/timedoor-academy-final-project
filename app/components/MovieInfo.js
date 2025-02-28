86688cd3dd369d3e8c6218bcb0bddfe579d46fcd
aryndraa <aryacoder1102@gmail.com>
2025-02-28 20:12:10 +0800

    movie info

--- app/components/MovieInfo.js
+++ app/components/MovieInfo.js
@@ -1,8 +1,8 @@
-export const MovieInfo = () => {
+export const MovieInfo = ({info, infoDetail}) => {
   return (
-    <div>
-      <h2>SU</h2>
-      <p>Semua Umur</p>
+    <div className="flex-col flex items-center">
+      <h2 className="text-2xl font-semibold">{info}</h2>
+      <p className="text-text-400 text-xs">{infoDetail}</p>
     </div>
   )
 }
--- app/movies/[movieId]/page.js
+++ app/movies/[movieId]/page.js
@@ -14,12 +14,12 @@ export default function Page ()  {
         </div>
         <div className="relative z-10 ">
           <div>
-            <Link href={"/movies"} className="absolute transform -translate-y-16 text-3xl p-2.5 rounded-full bg-dark-100 text-white ">
+            <Link href={"/movies"} className="absolute transform -translate-y-14 text-3xl p-2 rounded-full bg-white/40 text-white ">
                 <IoArrowBack/>
             </Link>
           </div>
-          <div className="flex flex-col mt-4">
-            <div className="flex flex-col items-center">
+          <div className="flex flex-col mt-2">
+            <div className="flex flex-col items-center mb-4">
               <img src="https://i.pinimg.com/736x/de/9a/e7/de9ae780aae37dbf947a56ad63ffd956.jpg"
                    alt="cover" 
                    className="w-[60%] h-[252px] object-cover rounded-lg mb-4"
@@ -27,8 +27,10 @@ export default function Page ()  {
               <h1 className="text-xl mb-1 font-semibold">Ruroini Kenshin</h1>
               <h2 className="text-text-400">Action</h2>
             </div>
-            <div>
-              <MovieInfo/>
+            <div className="grid grid-cols-3 border-y py-4 relative  border-border/20">
+              <MovieInfo info={"SU"} infoDetail={"Semua Usia"}/>
+              <MovieInfo info={"140"} infoDetail={"Minute"}/>
+              <MovieInfo info={"ID"} infoDetail={"Indonesia"}/>
             </div>
             
           </div>
