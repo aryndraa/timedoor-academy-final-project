7cc104d3efee6d8305b0addb30d8ea934d10667e
aryndraa <aryacoder1102@gmail.com>
2025-03-01 02:11:14 +0800

    book select location

---
+++ app/components/BookSelectLocation.js
@@ -0,0 +1,11 @@
+import { IoLocationSharp } from "react-icons/io5";
+
+export const BookSelectLocation = () => {
+  return (
+    <>
+      <button className="flex items-center py-3 px-4 text-sm font-semibold text-white bg-dark-100 gap-2 rounded-full">
+        Bali, Denpa.. <span className="text-lg"><IoLocationSharp/></span>
+      </button>
+    </>
+  )
+}
