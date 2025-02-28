85fba8a03a1037f49cc94157e286058eda7dd390
aryndraa <aryacoder1102@gmail.com>
2025-03-01 02:01:50 +0800

    movie book widget

--- app/movies/[movieId]/booking/page.js
+++ app/movies/[movieId]/booking/page.js
@@ -5,6 +5,16 @@ export default function Page ()  {
   return (
     <section>
       <BackBar/>
+      <div className="mt-4 bg-dark-100 p-3 rounded-lg flex gap-2 items-center">
+        <img 
+          src="https://i.pinimg.com/736x/de/9a/e7/de9ae780aae37dbf947a56ad63ffd956.jpg"
+          className='w-14 h-14 object-cover rounded-full'
+        />
+        <div>
+          <span className='text-sm'>Book Movie</span>
+          <h3 className='text-white font-semibold'>Ruroinin Kenshin</h3>
+        </div>
+      </div>
     </section>
   )
 }
