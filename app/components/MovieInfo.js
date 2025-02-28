a586929ab1b7fc5f82a5b0f79a460fcd70205976
aryndraa <aryacoder1102@gmail.com>
2025-03-01 00:58:13 +0800

    back button

--- app/components/MovieInfo.js
+++ app/components/MovieInfo.js
@@ -1,7 +1,7 @@
 export const MovieInfo = ({info, infoDetail}) => {
   return (
     <div className="flex-col flex items-center">
-      <h2 className="text-2xl font-semibold">{info}</h2>
+      <h2 className="text-xl font-semibold">{info}</h2>
       <p className="text-text-400 text-xs">{infoDetail}</p>
     </div>
   )
--- app/movies/[movieId]/page.js
+++ app/movies/[movieId]/page.js
@@ -12,27 +12,30 @@ export default function Page ()  {
           <img src="https://i.pinimg.com/736x/de/9a/e7/de9ae780aae37dbf947a56ad63ffd956.jpg" alt="hero" className="absolute left-0 right-0 top-0 max-h-[30vh]  min-w-full object-cover"/>
           <div className="min-h-[30vh] absolute top-0 left-0 right-0 bg-gradient-to-b from-dark-300/10  to-dark-300"></div>
         </div>
-        <div className="relative z-10 ">
+        <div className=" ">
           <div>
-            <Link href={"/movies"} className="absolute transform -translate-y-14 text-3xl p-2 rounded-full bg-white/40 text-white ">
+            <Link href={"/movies"} className="absolute left-0 right-0 top-0  text-3xl p-4 py-8  flex bg-gradient-to-b from-dark-300 to-dark-300/0 text-white  w-full ">
                 <IoArrowBack/>
             </Link>
           </div>
-          <div className="flex flex-col mt-2">
-            <div className="flex flex-col items-center mb-4">
-              <img src="https://i.pinimg.com/736x/de/9a/e7/de9ae780aae37dbf947a56ad63ffd956.jpg"
-                   alt="cover" 
-                   className="w-[60%] h-[252px] object-cover rounded-lg mb-4"
-              />
-              <h1 className="text-xl mb-1 font-semibold">Ruroini Kenshin</h1>
-              <h2 className="text-text-400">Action</h2>
+          <div className="relative z-10">
+            <div className="flex flex-col mt-2">
+              <div className="flex flex-col items-center mb-4">
+                <img src="https://i.pinimg.com/736x/de/9a/e7/de9ae780aae37dbf947a56ad63ffd956.jpg"
+                     alt="cover" 
+                     className="w-[60%] h-[260px] object-cover rounded-lg mb-4"
+                />
+                <h1 className="text-xl mb-1 font-semibold">Ruroini Kenshin</h1>
+                <h2 className="text-text-400">Action</h2>
+              </div>
+              <div>
+              </div>
+              <div className="grid grid-cols-3 border-y py-3 relative  border-border/20">
+                <MovieInfo info={"SU"} infoDetail={"Semua Usia"}/>
+                <MovieInfo info={"140"} infoDetail={"Minute"}/>
+                <MovieInfo info={"ID"} infoDetail={"Indonesia"}/>
+              </div>
             </div>
-            <div className="grid grid-cols-3 border-y py-4 relative  border-border/20">
-              <MovieInfo info={"SU"} infoDetail={"Semua Usia"}/>
-              <MovieInfo info={"140"} infoDetail={"Minute"}/>
-              <MovieInfo info={"ID"} infoDetail={"Indonesia"}/>
-            </div>
-            
           </div>
       </div>
       </section>
