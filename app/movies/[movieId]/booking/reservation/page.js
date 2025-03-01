ddba7030b226a35fbf554851594f1c8c0f97f0b2
aryndraa <aryacoder1102@gmail.com>
2025-03-02 02:24:57 +0800

    reservation page

--- app/movies/[movieId]/booking/reservation/page.js
+++ app/movies/[movieId]/booking/reservation/page.js
@@ -2,6 +2,7 @@ import { BackBar } from '@/app/components/BackBar'
 import { BottomBarCover } from '@/app/components/BottomBarCover'
 import { ReservationTime } from '@/app/components/ReservationTime'
 import { ReservationSeats } from '@/app/components/ReservationSeats'
+import Link from 'next/link'
 
 export default function Page () {
   return (
@@ -11,9 +12,10 @@ export default function Page () {
         <div className='mb-6'>
           <ReservationTime/>
         </div>
-        <div>
+        <div className='mb-6'>
           <ReservationSeats/>
         </div>
+        <Link href={"/movies/1/booking/payment"} className='bg-primary flex justify-center w-full p-4 rounded-lg text-dark-300 font-bold text-lg '>Booking</Link>
       </div>
       <BottomBarCover/>
     </section>
