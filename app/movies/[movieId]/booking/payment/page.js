e3df2c0ba9400fb65ee2fafc4914096516676bd0
aryndraa <aryacoder1102@gmail.com>
2025-03-04 21:45:34 +0800

    prisma install

--- app/movies/[movieId]/booking/payment/page.js
+++ app/movies/[movieId]/booking/payment/page.js
@@ -11,7 +11,7 @@ export default function page() {
 
       <div className='relative z-[70] mt-4 lg:mt-0 lg:flex w-full gap-20'>
         <div className='flex-1'>
-          <h1 className='text-xl lg:text-2xl  mb-6 font-medium text-text-200'>Booking</h1>
+          <h1 className='text-xl   mb-6 font-medium text-text-200'>Booking</h1>
           <TicketPaymentDetail/>
         </div>
         <div className='flex-1'>
