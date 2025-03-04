ab1e649446a1f3c462b91ccdef3b315468c3b4de
aryndraa <aryacoder1102@gmail.com>
2025-03-05 00:23:04 +0800

    Movie model

---
+++ api/app/Models/Movie.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Movie extends Model
+{
+    protected $fillable = [
+        'title',
+        'genre',
+        'age_restriction',
+        'duration',
+        'language',
+        'synopsis',
+        'author',
+        'start_date',
+        'end_date',
+    ];
+
+    public function casters(): HasMany {
+        return $this->hasMany(MovieCaster::class);
+    }
+}
