90e3e2ba5b95adb3462260a665c4b2f7385fa1f0
aryndraa <aryacoder1102@gmail.com>
2025-03-14 23:56:06 +0800

    cinema mobile

--- user2/src/components/cinemas/LocationDropdown.jsx
+++ user2/src/components/cinemas/LocationDropdown.jsx
@@ -12,64 +12,67 @@ export function LocationDropdown() {
   const [openSetCountry, setOpenSetCountry] = useState(false);
   const [openSetProvince, setOpenOpenSetProvince] = useState(false);
 
-  const handleSetCountry = () => {
-    setCountry("all");
-
-  }
-
   return (
     <div className="relative">
       <button
-        className="py-3 px-5 bg-dark-200 flex gap-3 items-center rounded-full capitalize font-medium"
+        className="text-base w-full lg:w-fit py-4 lg:py-3 px-5 bg-dark-200 flex gap-2 lg:gap-3 items-center rounded-lg lg:rounded-full capitalize font-medium"
         onClick={() => {
           setOpenSetCountry(!openSetCountry);
           setCountry("all")
           setProvince("all")
         }}
       >
-        <FaLocationDot />
+        <FaLocationDot className="text-base"/>
         {province !== "all"
           ? `${province}, ${country}`
           : "Pilih Lokasi"}
       </button>
 
-      <div className="absolute right-0 mt-4 flex flex-row-reverse gap-2">
-        <ul className={`bg-dark-250 rounded-lg h-fit w-52  ${
-          openSetCountry ? "block" : "hidden"
-        }`}>
-          {Object.keys(locations).map((flag) => (
-            <li key={flag}>
-              <button
-                className="hover:bg-dark-200 capitalize active:bg-dark-200 w-full h-full font-medium py-3 px-4 text-start first:rounded-t-lg last:rounded-b-lg"
-                onClick={() => {
-                  setCountry(flag)
-                  setOpenOpenSetProvince(true)
-                }}
-                type={"button"}
-              >
-                {flag.toUpperCase()}
-              </button>
-            </li>
-          ))}
-        </ul>
+      <div className="absolute right-0 left-0 mt-2 p-2 pb-4 lg:p-0 bg-dark-300 lg:mt-4 flex  flex-col lg:flex-row-reverse gap-2">
+        {
+          openSetCountry ?
+            <div>
+              <h3 className="lg:hidden my-2 text-sm text-text-400">Select Country</h3>
+              <ul className="bg-dark-200 lg:bg-dark-250 rounded-lg h-fit   w-full lg:w-52 shadow-lg ">
+                {Object.keys(locations).map((flag) => (
+                  <li key={flag}>
+                    <button
+                      className="hover:bg-dark-200  lg:text-base capitalize active:bg-dark-200 w-full h-full font-medium py-3 px-4 text-start first:rounded-t-lg last:rounded-b-lg"
+                      onClick={() => {
+                        setCountry(flag)
+                        setOpenOpenSetProvince(true)
+                      }}
+                      type={"button"}
+                    >
+                      {flag.toUpperCase()}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            : ""
+        }
 
         {openSetProvince ?
-          <ul className="bg-dark-250 rounded-lg w-52">
-            {locations[country].map((provinceOption) => (
-              <li key={provinceOption}>
-                <button
-                  className="hover:bg-dark-200 capitalize active:bg-dark-200 w-full h-full font-medium py-3 px-4 text-start first:rounded-t-lg last:rounded-b-lg"
-                  onClick={() => {
-                    setOpenOpenSetProvince(false)
-                    setOpenSetCountry(false)
-                    setProvince(provinceOption)
-                  }}
-                >
-                  {provinceOption}
-                </button>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <h3 className="lg:hidden my-2 text-sm text-text-400">Select Province</h3>
+            <ul className="bg-dark-200 lg:bg-dark-250 rounded-lg w-full lg:w-52 shadow-lg">
+              {locations[country].map((provinceOption) => (
+                <li key={provinceOption}>
+                  <button
+                    className="hover:bg-dark-200  text-nowrap capitalize active:bg-dark-200 w-full h-full font-medium py-3 px-4 text-start first:rounded-t-lg last:rounded-b-lg"
+                    onClick={() => {
+                      setOpenOpenSetProvince(false)
+                      setOpenSetCountry(false)
+                      setProvince(provinceOption)
+                    }}
+                  >
+                    {provinceOption}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
           :
           ""
         }
--- user2/src/pages/Cinemas.jsx
+++ user2/src/pages/Cinemas.jsx
@@ -6,11 +6,11 @@ export function Cinemas() {
   return (
     <>
       <section className="mx-3 lg:mx-20 mt-6 lg:mt-12 min-h-screen">
-        <div className="flex items-center justify-between">
+        <div className="flex gap-2 flex-col lg:flex-row lg:items-center justify-between">
           <SectionTitle title="All Cinemas" />
           <LocationDropdown/>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 mt-6 lg:mt-8">
           <CinemaCard/>
           <CinemaCard/>
           <CinemaCard/>
