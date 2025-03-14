437b75f9e1f5478322f7a3daeafaac3ceb59c796
aryndraa <aryacoder1102@gmail.com>
2025-03-14 20:58:31 +0800

    cinema mobile

--- user2/src/components/cards/CinemaCard.jsx
+++ user2/src/components/cards/CinemaCard.jsx
@@ -2,30 +2,30 @@ import { FaMapLocationDot, FaLocationDot, FaClock   } from "react-icons/fa6";
 
 export const CinemaCard = () => {
   return (
-    <div className="flex  gap-6 p-6 border border-border/20 bg-dark-250 hover:bg-dark-200 transition ease-in-out ">
+    <div className="flex gap-4 lg:gap-6 p-4 lg:p-6 border border-border/20 bg-dark-250 hover:bg-dark-200 transition ease-in-out ">
       <img
         src="https://fifthperson.com/wp-content/uploads/2018/05/lippo-kuta.jpg"
         alt=""
-        className="w-[200px] h-[200px] object-cover "
+        className="w-[120px] h-[120px] lg:w-[200px] lg:h-[200px] object-cover "
       />
       <div className="w-full   ">
-        <h2 className="text-xl font-medium pb-4  mb-4 border-b">Lippo Mall Bali Kuta</h2>
-         <div className="flex flex-col gap-8   justify-between">
+        <h2 className="lg:text-xl font-medium pb-3 mb-3 lg:pb-4  lg:mb-4 border-b">Lippo Mall Bali Kuta</h2>
+         <div className="flex flex-col gap-6 lg:gap-8   justify-between">
           <div className="flex flex-col gap-2">
-            <p className="flex items-center gap-2 text-text-400" >
+            <p className="flex text-xs font-medium lg:font-normal lg:text-base items-center gap-2 text-text-400" >
               <FaMapLocationDot/>
               Jln. Ngawi Selatan No. 12
             </p>
-            <p className="flex items-center gap-2 text-text-400" >
+            <p className="flex text-xs font-medium lg:font-normal lg:text-base items-center gap-2 text-text-400" >
               <FaLocationDot/>
               Indonesia / Bali
             </p>
-            <p className="flex items-center gap-2 text-text-400" >
+            <p className="flex text-xs font-medium lg:font-normal lg:text-base items-center gap-2 text-text-400" >
               <FaClock/>
               09:00 - 20:00
             </p>
           </div>
-          <div className="self-end flex gap-3">
+          <div className="self-end flex text-sm lg:text-base gap-3">
             <span>REGULER</span>
             <span>IMAX</span>
             <span>VIP</span>
--- user2/src/pages/Cinemas.jsx
+++ user2/src/pages/Cinemas.jsx
@@ -5,12 +5,12 @@ import {CinemaCard} from "../components/cards/CinemaCard.jsx";
 export function Cinemas() {
   return (
     <>
-      <section className="mx-20 mt-12 min-h-screen">
+      <section className="mx-3 lg:mx-20 mt-6 lg:mt-12 min-h-screen">
         <div className="flex items-center justify-between">
           <SectionTitle title="All Cinemas" />
           <LocationDropdown/>
         </div>
-        <div className="grid grid-cols-2 gap-8 mt-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-8">
           <CinemaCard/>
           <CinemaCard/>
           <CinemaCard/>
