d9766232541181dd51988b574608d409f97e8e2f
aryndraa <aryacoder1102@gmail.com>
2025-03-23 22:18:07 +0800

    carousel movie

--- static/src/components/home/Carousel.jsx
+++ static/src/components/home/Carousel.jsx
@@ -13,21 +13,51 @@ export const Carousel = () => {
       slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
-      autoplay={{ delay: 5000, disableOnInteraction: false }}
+      autoplay={{ delay: 10000, disableOnInteraction: false, pauseOnMouseEnter: true }}
       loop={true}
       className="w-full "
     >
       <SwiperSlide>
-        <CarouselItem />
+        <CarouselItem
+          cover={"https://mdentertainment.com/pictures/wp-content/uploads/2025/01/Pabrik-Gula-Web-Banner-MD-Website-750x1059px_11zon.jpg"}
+          genre={"horror"}
+          title={"Pabrik Gula"}
+          synopsis={"Endah, Fadhil, Dwi, Hendra, Wati, Ningsih, dan Franky berangkat bersama puluhan orang lainnya ke sebuah pabrik gula untuk menjadi....."}
+          duration={"134m"}
+          trailerUrl={"https://youtu.be/O76onpa-A7Y?si=JsNZOueI4waaF0lN"}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <CarouselItem />
+        <CarouselItem 
+          cover={"https://www.thatsmags.com/image/view/202502/NeCha-Cover-Image.jpg"}
+          title={"NE ZHA 2"}
+          genre={"animation"}
+          duration={"145m"}
+          synopsis={"Setelah sambaran petir surgawi, meskipun Ne Zha dan Ao Bing selamat dengan menjadi Roh, mereka akan segera menghilang sepenuhnya. Taiyi berencan....."}
+          trailerUrl={"https://youtu.be/nsXQijb0F4I?si=k_gaL96dcKqTjv1x"}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <CarouselItem />
+        <CarouselItem 
+          cover={"https://assets.pikiran-rakyat.com/crop/0x0:0x0/703x0/webp/photo/2025/01/25/1782199697.jpeg"}
+          duration={"134m"}
+          genre={"drama"}
+          synopsis={" Norma (Tissa Biani) yang pendiam dan pemalu merasa beruntung karena menikah dengan cinta pertamanya: Irfan (Yusuf Mahardika). Kehidupan pernikahan mereka...."}
+          title={"NORMA: ANTARA MERTUA DAN MENANTU"}
+          trailerUrl={"https://youtu.be/A4OjstdB-ao?si=3njMby5k0V0LsvCw"}
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <CarouselItem />
+        <CarouselItem 
+          title={"SNOW WHITE"}
+          genre={"FANTASY"}
+          duration={"104m"}
+          synopsis={"sebuah film musikal live-action yang digarap ulang berdasarkan film klasik tahun 1937, akan tayang secara eksklusif di bioskop pada bulan Maret...."}
+          cover={"https://4kwallpapers.com/images/walls/thumbs_3t/17907.jpeg"}
+          trailerUrl={"https://youtu.be/iV46TJKL8cU?si=tM15RzEfOX-JoUOs"}
+
+        />
+
       </SwiperSlide>
     </Swiper>
   );
--- static/src/components/home/CarouselItem.jsx
+++ static/src/components/home/CarouselItem.jsx
@@ -1,30 +1,46 @@
-import {Link} from "react-router-dom";
-import { FaPlay } from "react-icons/fa";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaPlay, FaTimes } from "react-icons/fa";
+import ReactPlayer from "react-player";
 
-export const CarouselItem = () => {
+export const CarouselItem = ({ cover, title, genre, synopsis, duration, trailerUrl }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="max-h-[70vh] flex ">
-      <div className=" hidden lg:flex flex-1 pl-20 max-w-lg bg-gradient-to-r from-dark-300 to-dark-300/0 flex-col  justify-center ">
-        <div className="mb-6">
-          <h1 className="text-3xl leading-[1.5] font-semibold mb-2 text-white">Captain America: Brave New World</h1>
+    <div className="max-h-[70vh] flex">
+      <div className="hidden lg:flex flex-1 pl-20 max-w-[28rem] bg-gradient-to-r from-dark-300 to-dark-300/0 flex-col justify-center">
+        <div className="mb-6 min-w-[29rem] z-10">
+          <h1 className="text-3xl leading-[1.5] font-semibold mb-2 text-white">{title}</h1>
           <div className="text-text-200/80 mb-2">
-            <span>Action</span> • 118
+            <span className="capitalize">{genre}</span> • {duration}
           </div>
-          <p className="text-text-200">Setelah bertemu dengan Presiden AS yang baru terpilih, Thaddeus Ross (Harrison Ford), Sam Wilson (Anthony Mackie), Captain America yang....</p>
+          <p className="text-text-200">{synopsis}</p>
         </div>
         <div className="flex items-center gap-2">
-          <Link to="/movies/:id" className="flex px-4 py-3 gap-4 items-center border border-primary rounded-lg font-semibold text-dark-300 bg-primary">Book Now</Link>
-          <Link to="/movies/:id" className="flex px-4 py-3 gap-4 items-center border border-primary rounded-lg font-semibold text-primary"><FaPlay/> Watch Trailer</Link>
+          <Link to={`/movies/${title}`} className="flex px-4 py-3 gap-4 items-center border border-primary rounded-lg font-semibold text-dark-300 bg-primary">
+            Book Now
+          </Link>
+          <button onClick={() => setIsOpen(true)} className="flex px-4 py-3 gap-4 items-center border border-primary rounded-lg font-semibold text-primary">
+            <FaPlay /> Watch Trailer
+          </button>
         </div>
       </div>
       <div className="relative flex-1 h-[28vh] lg:h-[70vh]">
         <div className="absolute bg-gradient-to-r from-dark-300 via-dark-300/0 to-transparent inset-0"></div>
-        <img
-          src="https://static1.cbrimages.com/wordpress/wp-content/uploads/sharedimages/2024/12/captain-america-brave-new-world-poster.jpg"
-          alt="a"
-          className="object-cover h-full w-full"
-        />
+        <img src={cover} alt={title} className="object-cover h-full w-full" />
       </div>
+
+      {/* Modal Trailer */}
+      {isOpen && (
+        <div className="fixed inset-0  flex items-center justify-center bg-black bg-opacity-70 z-50">
+          <div className="relative bg-dark-300 p-4 rounded-lg max-w-3xl w-full">
+            <button onClick={() => setIsOpen(false)} className="absolute top-3 right-3 text-white text-2xl">
+              <FaTimes />
+            </button>
+            <ReactPlayer url={trailerUrl} controls width="100%" height="400px" />
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
